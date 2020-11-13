3cb66a3786c776ea28d9bbe4734e3c1083bf9880
Victor <victor8730@ukr.net>
2020-11-14 00:17:19 +0200

    update migration and add README.md

---
+++ database/migrations/2020_11_13_215656_create_settings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('on_off_site');
+            $table->boolean('cache');
+            $table->boolean('gzip');
+            $table->boolean('ssl');
+            $table->string('robots', 100);
+            $table->text('title');
+            $table->text('description');
+            $table->text('keywords');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
