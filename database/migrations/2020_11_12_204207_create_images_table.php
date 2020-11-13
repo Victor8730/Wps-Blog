b07b0ad79b3750c854a94ccfc5721aa2668d9bba
Victor <victor8730@ukr.net>
2020-11-13 23:45:20 +0200

    add migration

---
+++ database/migrations/2020_11_12_204207_create_images_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('post_id')
+                ->unsigned()
+                ->index();
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts')
+                ->onDelete('cascade');
+            $table->string('slug', 50)
+                ->unique();
+            $table->string('image');
+            $table->string('alt');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
